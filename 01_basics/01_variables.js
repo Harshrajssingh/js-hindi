fb5d514ae93afba6adf324c6a8da13cffad77788
Harshrajssingh <166421373+Harshrajssingh@users.noreply.github.com>
2025-06-04 06:41:57 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "harsh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 
+accountEmail = "ha@ha.com"
+accountPassword = "54321"
+accountCity = "Jamshedpur"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
